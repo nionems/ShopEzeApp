9c6c6531ce62075a54e06baa27ffff1425aa57c3
lionel <nionems@icloud.com>
2023-05-10 17:34:02 +1000

    screens files are created

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+//screens
+import { HomeScreen } from './screens/HomeScreen';
+import { SignUpScreen } from './screens/SignUpScreen';
+import { SignInScreen } from './screens/SignInScreen';
+import { MealPlanScreen } from './screens/MealPlanScreen';
+import { RecipeScreen } from './screens/RecipeScreen';
+import { BarCodeScreen } from './screens/BarCodeScreen';
+import { SettingScreen } from './screens/SettingScreen';
+
+
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>this is ready to go 
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Signup" component={SignUpScreen}/>
+        <Stack.Screen name="Signin" component={SignInScreen}/>
+        <Stack.Screen name="Mealplan" component={MealPlanScreen}/>
+        <Stack.Screen name="Recipe" component={RecipeScreen}/>
+        <Stack.Screen name="Barcode" component={BarCodeScreen}/>
+        <Stack.Screen name="Setting" component={SettingScreen}/>
 
-        </Text>
-        <StatusBar style="auto" />
-      </View>
+
+
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
