2e8753a3ca8b870d135ccb9c0a811464faaa383c
lionel <nionems@icloud.com>
2023-05-10 16:07:15 +1000

    git setup

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app lalalalla!nfjklfklvnfklvfklmvlkc
+        <Text>this is ready to go 
 
         </Text>
         <StatusBar style="auto" />
