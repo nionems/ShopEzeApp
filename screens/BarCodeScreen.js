b0fa39eea0ef8672ce8c3ad89e53c4680c8bf212
lionel <nionems@icloud.com>
2023-07-04 15:41:53 +1000

    mealplan as been created but need to be connected to DB

--- screens/BarCodeScreen.js
+++ screens/BarCodeScreen.js
@@ -1,31 +1,126 @@
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+//import axios from 'axios';
 
 export function BarCodeScreen(props) {
-    return (
-        <View style={styles.page}>
-            <View style={styles.header}>
-                <Text style={styles.headerTitle}>Scan</Text>
-            </View>
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [barcodeData, setBarcodeData] = useState(null);
+
+  const handleBarCodeScanned = async ({ type, data }) => {
+    setScanned(true);
+    setBarcodeData({ type, data });
+  
+    try {
+      const response = await axios.get(`YOUR_API_ENDPOINT?barcode=${data}`);
+      const productData = response.data; // Adjust this line based on the API response structure
+  
+      // Update the state or perform any additional logic with the productData
+      // For example, you can set it to a productInfo state variable:
+      setProductInfo(productData);
+    } catch (error) {
+      // Handle any errors that occur during the API request
+      console.log('Error fetching product data:', error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+//   const handleBarCodeScanned = ({ type, data }) => {
+//     setScanned(true);
+//     setBarcodeData({ type, data });
+//   };
+
+  const scanAgain = () => {
+    setScanned(false);
+    setBarcodeData(null);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      {scanned && (
+        <View style={styles.overlay}>
+          <Text style={styles.scanDataText}>{barcodeData?.data}</Text>
+          <Text style={styles.scanTypeText}>{barcodeData?.type}</Text>
+          <Button title="Scan Again" onPress={scanAgain} />
         </View>
-    )
+      )}
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-    page: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    header: {
-        backgroundColor: "#26ACA7",
-        marginTop: 50,
-        height: 70,
-        minWidth: 400,
-    },
-    headerTitle: {
-        fontSize: 40,
-        marginTop: 25,
-        textAlign: 'center',
-        color: "#FD8749",
-        fontStyle: "italic",
-        fontWeight: "bold"
-    },
-})
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: '50%',
+    left: '10%',
+    right: '10%',
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    borderRadius: 10,
+  },
+  scanDataText: {
+    fontSize: 20,
+    color: '#fff',
+    marginBottom: 10,
+  },
+  scanTypeText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
+
+// import { View, Text, StyleSheet } from "react-native";
+
+// export function BarCodeScreen(props) {
+//     return (
+//         <View style={styles.page}>
+//             <View style={styles.header}>
+//                 <Text style={styles.headerTitle}>Scan</Text>
+//             </View>
+//         </View>
+//     )
+// }
+// const styles = StyleSheet.create({
+//     page: {
+//         justifyContent: "center",
+//         alignItems: "center",
+//     },
+//     header: {
+//         backgroundColor: "#26ACA7",
+//         marginTop: 50,
+//         height: 70,
+//         minWidth: 400,
+//     },
+//     headerTitle: {
+//         fontSize: 40,
+//         marginTop: 25,
+//         textAlign: 'center',
+//         color: "#FD8749",
+//         fontStyle: "italic",
+//         fontWeight: "bold"
+//     },
+// })
--- screens/MealPlanScreen.js
+++ screens/MealPlanScreen.js
@@ -1,33 +1,155 @@
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { Text, View, TextInput, Button, StyleSheet,ScrollView } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
-export function MealPlanScreen(props) {
-    return (
+export  function MealPlanScreen(props) {
+  const [meal, setMeal] = useState('');
+  const [ingredient, setIngredient] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+  const [ingredientsByDate, setIngredientsByDate] = useState({});
+
+  const handleAddIngredient = () => {
+    // Implement your logic to add the event to the meal plan
+    // For example, you can update a state variable or make an API call
+    const updatedIngredients = { ...ingredientsByDate };
+
+    if (updatedIngredients[selectedDate]) {
+      updatedIngredients[selectedDate].push({ meal, ingredient });
+    } else {
+      updatedIngredients[selectedDate] = [{ meal, ingredient }];
+    }
+
+    setIngredientsByDate(updatedIngredients);
+    console.log('Adding ingredient:', { meal, ingredient, selectedDate });
+    setMeal('');
+    setIngredient('');
+  };
+
+  const handleDateSelect = (day) => {
+    setSelectedDate(day.dateString);
+  };
+
+  const renderIngredientsForDate = () => {
+    if (!selectedDate || !ingredientsByDate[selectedDate]) {
+      return null;
+    }
+
+    return ingredientsByDate[selectedDate].map((item, index) => (
+      <View key={index} style={styles.ingredientContainer}>
+        <Text style={styles.mealText}>Meal: {item.meal}</Text>
+        <Text style={styles.ingredientText}>Ingredient: {item.ingredient}</Text>
+      </View>
+    ));
+  };
+
+  return (
+      <ScrollView>
+    <View style={styles.container}>
         <View style={styles.page}>
-            <View style={styles.header}>
-                <Text style={styles.headerTitle}>Meal Plan</Text>
-            </View>
-        </View>
-    )
+             <View style={styles.header}>
+                 <Text style={styles.headerTitle}>Meal Plan</Text>
+             </View>
+         </View>
+         <Calendar
+        style={styles.calendar}
+        onDayPress={handleDateSelect}
+        markedDates={{ [selectedDate]: { selected: true } }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a meal"
+        value={meal}
+        onChangeText={setMeal}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter ingredient"
+        value={ingredient}
+        onChangeText={setIngredient}
+      />
+      <Button  title="Add Event" onPress={handleAddIngredient} color="#26ACA7" />
+
+      <Text style={styles.selectedDate}>
+         {selectedDate ? new Date(selectedDate).toDateString() : ''}
+      </Text>
+      <View style={styles.ingredientContainer}>{renderIngredientsForDate()}</View>
+    </View>
+    </ScrollView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    page: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    header: {
-        backgroundColor: "#26ACA7",
-        marginTop: 50,
-        height: 70,
-        minWidth: 400,
-    },
-    headerTitle: {
-        fontSize: 40,
-        marginTop: 25,
-        textAlign: 'center',
-        color: "#FD8749",
-        fontStyle: "italic",
-        fontWeight: "bold"
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#26ACA7',
+    marginTop:3,
+    paddingHorizontal: 10,
+    borderRadius:10,
+  },
+  selectedDate: {
+    marginTop: 10,
+    fontSize: 18,
+    color:"#26ACA7",
+    fontStyle:"bold"
+  },
+//   ingredientsTitle: {
+//     marginTop: 20,
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//   },
+  
+  ingredientContainer: {
+    marginBottom: 10,
+  },
+  ingredientText: {
+    fontSize: 16,
+    marginBottom:0,
+    color:"#FD8749",
+},
+mealText: {
+    fontSize: 16,
+    marginBottom:0,
+    color:"#FD8749",
+},
+header: {
+    backgroundColor: "#26ACA7",
+    marginTop: 50,
+    height: 70,
+    minWidth: 400,
+},
+headerTitle: {
+    fontSize: 40,
+    marginTop: 25,
+    textAlign: 'center',
+    color: "#FD8749",
+    fontStyle: "italic",
+    fontWeight: "bold"
+},
+calendar: {
+    marginBottom: 10,
+    backgroundColor:"#26ACA7",
+    marginTop:10,
+    minWidth:400,
+    
+  },
+
+  
+
 })
+    
+    
+
+
